2ff71562b19f63f2310e694b51ffd0aa07f0a9f8
AndresOrtega3573 <117954700+AndresOrtega3573@users.noreply.github.com>
2023-12-05 21:56:05 -0500

    Creacion Hola Mundo
    Hola Mundo

---
+++ HolaMundo/src/uisrael/poo/lab1/HolaMundo.java
@@ -0,0 +1,12 @@
+package uisrael.poo.lab1;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        //RESPUESTA: En Eclipse, los archivos '.class' se generan automáticamente
+        // cuando se compila el código fuente de tu proyecto. A diferencia
+        // del modo consola, Eclipse proporciona un entorno de desarrollo
+        // integrado (IDE) con una interfaz gráfica que facilita la gestión y
+        // compilación de proyectos Java.
+    }
+}
